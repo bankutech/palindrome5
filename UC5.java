e86f4519e89954fbca1f1b35353b614f8f36a50a
Sagnik Mitra <sagnikmitra1302@gmail.com>
2026-03-06 21:18:47 +0530

    Stack-Based Palindrome Checker

--- UC5.java
+++ UC5.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+import java.util.Stack;
 
+public class UseCase5PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is not a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
